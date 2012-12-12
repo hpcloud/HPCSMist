00a75366811dd154cd3208ded87130e886d24df8
Mike Hagedorn <mike.hagedorn@hp.com>
2012-12-12 14:15:11 -0600

    moved source around

---
+++ HPCSMist/HPCSMist/HPCSMist.h
@@ -0,0 +1,13 @@
+//
+//  HPCSMist.h
+//  HPCSMist
+//
+//  Created by Mike Hagedorn on 12/12/12.
+//  Copyright (c) 2012 Mike Hagedorn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HPCSMist : NSObject
+
+@end
---
+++ HPCSMist/HPCSMistTests/HPCSMistTests.h
@@ -0,0 +1,13 @@
+//
+//  HPCSMistTests.h
+//  HPCSMistTests
+//
+//  Created by Mike Hagedorn on 12/12/12.
+//  Copyright (c) 2012 Mike Hagedorn. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HPCSMistTests : SenTestCase
+
+@end
