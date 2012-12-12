ea7f385a98904e22f39b064cd78c616066d7ffe3
Mike Hagedorn <mike.hagedorn@hp.com>
2012-12-12 15:21:04 -0600

    project reorg for workspace prep

---
+++ Classes/Controllers/HPCSViewController.m
@@ -0,0 +1,29 @@
+//
+//  HPCSViewController.m
+//  HPCSMist
+//
+//  Created by Mike Hagedorn on 12/12/12.
+//  Copyright (c) 2012 Mike Hagedorn. All rights reserved.
+//
+
+#import "HPCSViewController.h"
+
+@interface HPCSViewController ()
+
+@end
+
+@implementation HPCSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Other Sources/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HPCSMist
+//
+//  Created by Mike Hagedorn on 12/12/12.
+//  Copyright (c) 2012 Mike Hagedorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HPCSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HPCSAppDelegate class]));
+    }
+}
