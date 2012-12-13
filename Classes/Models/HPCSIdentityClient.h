23eed86914e5705d7ce3c5a51c263cb1d3004044
Mike Hagedorn <mike.hagedorn@hp.com>
2012-12-13 12:56:21 -0600

    documentation fixes for IdentityClient header

--- Classes/Models/HPCSIdentityClient.h
+++ Classes/Models/HPCSIdentityClient.h
@@ -15,25 +15,14 @@
 #import "HPCSTenant.h"
 
 
-
-/**
- Indicates an error occured in AFNetworking.
-
- @discussion Error codes for HPCSNetworkingErrorDomain correspond to codes in NSURLErrorDomain.
- */
 extern NSString * const HPCSNetworkingErrorDomain;
 
-/**
- Posted when an authentication fails.
- */
 extern NSString * const HPCSAuthenticationDidFailNotification;
 
 extern NSString * const HPCSKeystoneNovaCatalogIsEmptyNotification;
 extern NSString * const HPCSKeystoneSwiftCatalogIsEmptyNotification;
 
-/** Posted when the username or password or tenantId are changed
-* this needs to get done so that the IdentityClient knows to dump its cached token
-*/
+
 extern NSString * const HPCSKeystoneCredentialsDidChangeNotification;
 
 extern NSString * const kHPCSAuthUsernameKey;
@@ -60,46 +49,47 @@ extern NSString * const kHPCSAuthSecretKey;
 /** Whether or not the current token is valid */
 @property (assign) BOOL isTokenValid;
 
-/** The access key for this user */
+/** The access key for this user 
+ @discussion will be nil if sharedClient is utilized for intitialization
+ */
 @property (retain) NSString *accessKeyId;
 
-/** The access key for this user */
+/** The access key for this user 
+ 
+ @discussion will be nil if sharedClient is utilized for intitialization
+ */
 @property (retain) NSString *secretKey;
 
+///-----------------------------------------------------
+/// @name Creating and Initializing HPCSIdentity Clients
+///-----------------------------------------------------
 
 
 /** Convenience method to get the HPCSIdentityClient 
-    
-    returns a singleton
  
-    @discussion if you use this method, then you are using the initWithUsername:andPassword:andTenantId method to constuct the identity client instance.  
+ @discussion Returns a singleton. If you use this method, then you are using the initWithUsername:andPassword:andTenantId: method to constuct the identity client instance, as opposed to the initWithAccessKeyId:andSecretKey:andTenantId: method.
  */
 +(HPCSIdentityClient *)sharedClient;
 
 /** Initialize using username and password
- 
- HP Cloud Services allows either token and id based login or username and password based login
- 
- @discussion this violates the notion of a designated initializer...
+
  @param userName  The name that you use to login to the Management Console with
  @param password The password that you use to login to the Management Console with
  @param tenantId Your tenantId for your account
- 
- 
+ @discussion HP Cloud Services allows either AccessKey and SecretKey based login or username and password based login.  sharedClient calls this one.
  */
 -(id) initWithUsername:(NSString *)userName andPassword:(NSString *)password andTenantId:(NSString *)tenantId;
 
 /** Initialize using accessKey and secret key
  
- HP Cloud Services allows either token and id based login or username and password based login
- 
- @discussion this violates the notion of a designated initializer...
  @param accessKey  Your access key
  @param secretKey  Your secret key
  @param tenantId Your tenantId for your account
- 
+ @discussion HP Cloud Services allows either AccessKey and SecretKey based login or username and password based login.
+
  
  */
+
 
 -(id) initWithAccessKeyId:(NSString *)accessKey andSecretKey:(NSString *)secretKey andTenantId:(NSString *)tenantId;
 
@@ -118,8 +108,18 @@ extern NSString * const kHPCSAuthSecretKey;
  
  
  */
+
+///------------------------------------------------------
+/// @name Authenticating with Control Services (Keystone)
+///------------------------------------------------------
+
 //TODO need to send a message about network not available
 -(void) authenticate:(void (^)(NSArray *serviceCatalog))block failure:(void (^)(NSHTTPURLResponse *responseObject, NSError *error))failure;
+
+
+///------------------------------------------------------
+/// @name Checking Your Authorization Status
+///------------------------------------------------------
 
 /** Whether or not your HPCSToken is expired */
 -(BOOL) isTokenExpired;
@@ -127,20 +127,23 @@ extern NSString * const kHPCSAuthSecretKey;
 /** Whether or not you are logged in */
 -(BOOL) isAuthenticated;
 
-/** Invalidate your current token 
-    
-    allows you to manually invalidate your current token
-    
-    @param success block called if this operation succeeds
-    @param failure block called if this operation fails
- 
- 
+///----------------------------
+/// @name Ending Your Session
+///----------------------------
+
+/** Invalidate your current token
+
+ @param success block called if this operation succeeds
+ @param failure block called if this operation fails
+ @discussion  allows you to manually invalidate your current token/session on the Control Services server, this does not delete the local cached token, and that token will be invalid after the success of this method.
 */
 -(void) tokenInvalidate:(void (^)(NSHTTPURLResponse *response)) success failure:(void (^)(NSHTTPURLResponse *response, NSError *error))failure;
 
-/** Retrieve an instance of the Compute Client 
-    
-    designated way to get and instance of the compute client
+///-----------------------------------------------
+/// @name Getting Access To Authenticated Services
+///-----------------------------------------------
+
+/** Retrieve an instance of the Compute Client,designated way to get an instance of the Compute (Nova) client
     
 @returns nil if no compute resource is in the service catalog
 @discussion if the service catalog does not contain a compute service for this user the HPCSKeystoneNovaCatalogIsEmptyNotification is sent
@@ -150,27 +153,29 @@ Normal Response Code(s): 204
 Error Response Code(s): unauthorized (401), forbidden (403), badRequest (400))
  
  */
+
+
 -(HPCSComputeClient *)computeClient;
 
 /** the URL for the compute endpoint as listed in the Service Catalog */
 -(NSString *)publicUrlForCompute;
 
-/** Retrieve an instance of the Object Storage  Client
- 
- designated way to get and instance of the object storage (Swift) client
+/** Retrieve an instance of the Object Storage Client, designated way to get an instance of the object storage (Swift) client
  
  @discussion if the service catalog does not contain a compute service for this user the HPCSKeystoneSwiftCatalogIsEmptyNotification is sent.
  
  */
-
-
 -(HPCSSwiftClient *)swiftClient;
 
 
 /** the URL for the object storage endpoint as listed in the Service Catalog */
 -(NSString *)publicUrlForObjectStorage;
 
-/** Stores the user name in the UserDefaults storage object 
+///-----------------------------------------------
+/// @name Secure Management of Your Credentials
+///-----------------------------------------------
+
+/** Stores the user name in the secure Keychain 
  @param userName your username
  */
 -(void) setUsername:(NSString*)userName;
@@ -185,23 +190,24 @@ Error Response Code(s): unauthorized (401), forbidden (403), badRequest (400))
 */
 -(void) setPassword:(NSString *)password;
 
-/** Get the password from the secure keychain */
+/** Get the password from the secure Keychain */
 -(NSString *)password;
 
-/** Store the tenantId in UserDefaults
+/** Store the tenantId in the secure Keychain
  @param tenantId the tenantId to store
  */
 -(void)setTenantId:(NSString *)tenantId;
 
-/** Get the tenantId from userDefaults */
+/** Get the tenantId from storage */
 -(NSString *)tenantId;
 
 /** Get the token from the Keychain */
 -(HPCSToken *)token;
 
-/** Store the token in the keychain 
-* @discussion pass in nil to delete token from Keychain
-  @param token the token object to store in the Keychain
+/** Store the token in the secure Keychain 
+
+@param token the token object to store in the Keychain
+@discussion pass in nil to delete token from Keychain, this will cause a relogin to occur the next time an authenticated service is called.  This essentially deletes the local cache of the token.
 */
 -(void) setToken:(HPCSToken *)token;
 
