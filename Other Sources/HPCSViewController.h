2c34a38f8d2e006007db986bfbabd4fe2d434fd4
Mike Hagedorn <mike@silverchairsolutions.com>
2013-03-28 09:41:51 -0500

    added back dummy VC

---
+++ Other Sources/HPCSViewController.h
@@ -0,0 +1,13 @@
+//
+//  HPCSViewController.h
+//  HPCSMist
+//
+//  Created by Mike Hagedorn on 3/28/13.
+//  Copyright (c) 2013 Mike Hagedorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPCSViewController : UIViewController
+
+@end
---
+++ Other Sources/HPCSViewController.m
@@ -0,0 +1,38 @@
+//
+//  HPCSViewController.m
+//  HPCSMist
+//
+//  Created by Mike Hagedorn on 3/28/13.
+//  Copyright (c) 2013 Mike Hagedorn. All rights reserved.
+//
+
+#import "HPCSViewController.h"
+
+@interface HPCSViewController ()
+
+@end
+
+@implementation HPCSViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
